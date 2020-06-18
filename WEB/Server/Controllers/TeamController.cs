99bcd196dd78afe9a211e435bc2fd26b5ee11860
AlbertPC <aj.groenwold@gmail.com>
2020-06-19 00:14:04 +0200

    first build02 push

---
+++ WEB/Server/Controllers/TeamController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WEB.Server.src;
+using WEB.Server.Utilities;
+using WEB.Shared;
+
+namespace WEB.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeamController : ControllerBase
+    {
+        private readonly ILogger<TeamController> logger;
+
+        public TeamController(ILogger<TeamController> logger)
+        {
+            this.logger = logger;
+        }
+
+        private string getData(DataTable table, int iteration, string col)
+        {
+            return table.Rows[iteration-1][col].ToString();
+        }
+
+        [HttpGet]
+        public IEnumerable<TeamShared> Get()
+        {
+            DataManager dm = new DataManager();
+
+            DataTable table = dm.getDBTables("Teams");
+
+            int numOfTeams = table.Rows.Count;
+
+            return Enumerable.Range(1, numOfTeams).Select(index => new TeamShared
+            {
+                teamID = getData(table,index,"TeamID"),
+                teamName = getData(table, index, "TeamName"),
+                teamAdmin = getData(table, index, "Admin").Equals("1") ? "True" : "False",
+                teamDev = getData(table, index, "Developer").Equals("1") ? "True" : "False",
+                teamTest = getData(table, index, "Tester").Equals("1") ? "True" : "False"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WEB/Server/src/Team.cs
@@ -0,0 +1,30 @@
+﻿using WEB.Server.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace WEB.Server.src
+{
+    class Team
+    {
+
+       // public DataTable getTeamInfo(string colNameToSearch, string colValueToSearch, string colValueToReturn)
+        //{
+        //    DataTable table = null;
+//
+        //    DataManager dm = new DataManager();
+//
+        //    table.Load(dm.getDBInfo("Teams", colNameToSearch, colValueToSearch, colValueToReturn));
+//
+        //}
+        //public string getUserInfo(string colNameToSearch, string colValueToSearch, string colValueToReturn)
+        //{
+          //  DataManager dm = new DataManager();
+//
+         //   return dm.getDBInfo("Users", colNameToSearch, colValueToSearch, colValueToReturn);
+//
+        //}
+
+    }
+}
